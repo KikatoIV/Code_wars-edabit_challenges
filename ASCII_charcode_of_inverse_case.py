cb4e3277a4ee944267630a5df177f5b1e9e580de
KikatoIV <39209669+KikatoIV@users.noreply.github.com>
2020-07-02 09:30:36 +0200

    Delete ASCII_charcode_of_inverse_case.py

--- ASCII_charcode_of_inverse_case.py
+++
@@ -1,8 +0,0 @@
-def counterpartCharCode(char):
-    if str(char).isupper():
-        return ord(str(char.lower()))
-    if str(char).islower():
-        return ord(str(char.upper()))
-    else:
-        return ord(char)
-print(counterpartCharCode('L'))
